c9ed86bb12b9e83f844dc6281623109f0d9086b6
christiankopper5 <ch.kopper1@gmail.com>
2023-09-27 10:09:27 +0200

    Create Hodina 3g.py
    Programovanie H 3

---
+++ Hodina 3g.py
@@ -0,0 +1,17 @@
+meno = input("Ako sa volas?")
+print("ahoj", meno)
+
+den = input("Aky si mal den?")
+print("je dobre ze si mal",den,"den")
+
+vek = input("Kolko mas rokov")
+print("takze mas",vek,"rokov")
+
+vec = input("co bdues dnes robit ")
+print("nic lepsie ako",vec,"neni")
+
+farby = input("aka je tvoja oblubena farba")
+print(farby, "tak to je nahovno farba ruzova lepsia")
+
+priklad =int (input("Zadaj cislo ktorte chces nasobit *4"))
+print("Visledok je:",priklad*4)
