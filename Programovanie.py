22dda4c67ccdfebd91d308abad415ea9ffd2217b
christiankopper5 <ch.kopper1@gmail.com>
2023-09-27 10:41:12 +0200

    Create Programovanie.py
    Hodina 2

---
+++ Programovanie.py
@@ -0,0 +1,20 @@
+#moj prvy program
+#print ("vitaj")
+#print()
+#print("vypocet je",3 + 5 *5)
+#print("nazdar")
+#print("moje meno je Chris")
+#print("moj vek je",15)
+#print ("jezisko neexistuje")
+#print ("vypocitaj prikld 5 + 6 *3")
+
+
+
+#print("vysledok je",5 + 6)
+
+a=int (input("napis dake cislo"))
+b= 5
+c= a+b
+print(c)
+      
+      
